2162f3753836deee5b97b7289fcfb97307d5226c
fontcuberta <isafontcu@gmail.com>
2015-04-07 20:04:39 +0200

    room.rb reimplementation, quitting when the user wants to quit

--- room.rb
+++ room.rb
@@ -1,3 +1,4 @@
+require 'colorize'
 class Room
 	attr_accessor :exit
 	attr_accessor :rightmove
@@ -13,6 +14,15 @@ class Room
 		@description = description
 		@death = death
 		@gun = ""
+	end
+
+	def where
+		puts "You are in the #{description}. There is an exit at #{exit}.\n>"
+		return gets.chomp
+	end
+
+	def get_out
+		puts rightmove
 	end
 end
 
@@ -31,17 +41,62 @@ class Enviroment
 	end
 
 	def print_inventory
+		puts "Hum!!! In your pocket you will find: \n"
 		@Inventory.each_with_index do |gun, index|
 			puts "#{index+1}) #{gun}"
 		end
 	end
 
 	def drop
-		p = print_inventory
-		puts "Select the number of the object you want to drop"
-		obj_index = gets.chomp.to_i
-		puts "You just dropped the #{@Inventory[obj_index-1]}"
-		@Inventory.delete_at(obj_index)
+		if @Inventory == []
+			puts "Ooooops!!! Your pockets are empty -.- "
+		else
+			p = print_inventory
+			puts "Select the number of the object you want to drop"
+			obj_index = gets.chomp.to_i
+			puts "You just dropped the #{@Inventory[obj_index-1]}"
+			a = @Inventory[obj_index-1]
+			@Inventory.delete_at(obj_index)
+			return a
+		end
+
+	end
+
+	def playing
+		@path.each do |room|
+			loop do
+				answer = room.where
+					case answer
+					when "N", "E", "S", "W"
+						if answer == room.exit
+							room.get_out
+							break
+						else
+							puts room.wrongmove
+						end
+					when "pickup"
+						if room.gun!=""
+							a = pickup(room.gun)
+							p = print_inventory
+							room.gun = ""
+						else
+							puts ("There is nothing here!").colorize(:red)
+						end
+					when "drop"
+						a = drop
+					when "quit", "exit"
+						puts "Bye, bye... quitter!!!"
+						exit
+					else
+						puts ("So sorry... I don't understand your babbling!!!. Try again").colorize(:yellow)
+					end
+			end
+		end
+	puts "WINNEEEEEERRRRRRR!!!"
+	end
+
+	def treeofchoices
+
 	end
 
 end
@@ -57,7 +112,4 @@ room_cat.gun = "Magnum"
 myLaby.add_room(room_cow)
 myLaby.add_room(room_dog)
 myLaby.add_room(room_cat)
-myLaby.pickup("Sword")
-myLaby.pickup("Arrow & Bow")
-myLaby.pickup("Gunshot")
-myLaby.drop
+myLaby.playing
