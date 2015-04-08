8a9ede63e361f03be59a5c3ac0aeab2e41948eb7
fontcuberta <isafontcu@gmail.com>
2015-04-08 20:26:01 +0200

    Prototype terminal.rb Exercise Day 3

--- terminal.rb
+++ terminal.rb
@@ -1,9 +1,49 @@
 require 'terminfo'
 
 class Terminal
+	def calculate
+	end
+
+	def show
+	end
 	def execute
-		puts TermInfo.screen_size
+		puts "\n"*(TermInfo.screen_size[0]/2)                        
+		printf(CalculateWidth.new.execute, "")
+		sleep 2
+		system("clear")
 	end
 end
 
-Terminal.new.execute
+class CalculateWidth
+	def execute
+		"%"+(TermInfo.screen_size[1]/2).to_s+"s"
+	end
+end
+
+
+class Show_slide
+	def initialize
+		@slides = ["Prepare for the next \033[1mbig\033[0m thing", "The iTerminal", "Is here"]
+	end
+	def execute
+		system("clear")
+		sleep 2
+		@slides.each do |slide|
+			puts "\n"*(TermInfo.screen_size[0]/2)
+			printf(CalculateWidth.new.execute, slide)
+			sleep 2
+			system("clear")
+		end
+	end
+end
+
+class Move
+	def foward
+	end
+	def backward
+	end
+end
+
+
+
+Show_slide.new.execute
